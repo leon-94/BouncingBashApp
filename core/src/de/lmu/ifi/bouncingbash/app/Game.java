741172f05f71e4fe7e60370cba1031c7b53a7345
torantie <michi.maeh@googlemail.com>
2015-12-29 23:30:46 +0100

    Bilder und Steuerung des Spiels

--- core/src/de/lmu/ifi/bouncingbash/app/Game.java
+++ core/src/de/lmu/ifi/bouncingbash/app/Game.java
@@ -8,11 +8,17 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
@@ -34,7 +40,7 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 	private Sprite spritePlatform;
 	private GameModel gameModel;
 	private Ball b;
-	private Body body1,body2;
+	private Body body1,body2,bodyEdgeScreen;
 
 	private World world;
 
@@ -47,7 +53,7 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 		b.setyCoordinates(gameModel.getMap().getMainPlatform().getHeight());
 		//kontrolliert touch input TODO Accelerometer Abfrage einfügen
 		//
-		world = new World(new Vector2(0, -30f),true);
+		world = new World(new Vector2(0, -100f),true);
 
 		batch = new SpriteBatch();
 		textureBall = gameModel.getPlayer().getBall().getTexture();
@@ -59,18 +65,6 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 		texturePlatform = gameModel.getMap().getMainPlatform().getTexture();
 		spritePlatform = new Sprite(texturePlatform);
 		System.out.println("Game: " + Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer));
-
-		// Sprite1's Physics body
-		BodyDef bodyDef = new BodyDef();
-		bodyDef.type = BodyDef.BodyType.DynamicBody;
-		bodyDef.position.set(spriteBall.getX() , spriteBall.getY() );
-		body1= world.createBody(bodyDef);
-
-		// Sprite2's physics body
-		BodyDef bodyDef2 = new BodyDef();
-		bodyDef2.type = BodyDef.BodyType.DynamicBody;
-		bodyDef2.position.set(spritePlatform.getX() , spritePlatform.getY() );
-		body2 = world.createBody(bodyDef2);
 		//TODO
 		//Controller c = new Controller(gameModel,body1);
 		Gdx.input.setInputProcessor(this);
@@ -80,6 +74,17 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 		PolygonShape shape2 = new PolygonShape();
 		shape2.setAsBox(spritePlatform.getWidth()/2 , spritePlatform.getHeight() /2 );
 
+		// Sprite1's Physics body
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyDef.BodyType.DynamicBody;
+		bodyDef.position.set(spriteBall.getX() , spriteBall.getY() );
+		body1= world.createBody(bodyDef);
+
+		// Sprite2's physics body
+		BodyDef bodyDef2 = new BodyDef();
+		bodyDef2.type = BodyDef.BodyType.StaticBody;
+		bodyDef2.position.set(spritePlatform.getX(), spritePlatform.getY());
+		body2 = world.createBody(bodyDef2);
 
 		FixtureDef fixtureDef = new FixtureDef();
 		fixtureDef.shape = shape1;
@@ -88,16 +93,64 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 		Fixture fixture = body1.createFixture(fixtureDef);
 		Fixture fixture2 = body2.createFixture(fixtureDef);
 
+
+		BodyDef bodyDef3 = new BodyDef();
+		bodyDef3.type = BodyDef.BodyType.StaticBody;
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		bodyDef3.position.set(0,0);
+
+		FixtureDef fixtureDef2 = new FixtureDef();
+		EdgeShape edgeShape = new EdgeShape();
+		edgeShape.set(-w/2,0,w/2,0);
+		System.out.println("w: "+w+" h: "+h);
+		fixtureDef2.shape = edgeShape;
+
+		bodyEdgeScreen = world.createBody(bodyDef3);
+		bodyEdgeScreen.createFixture(fixtureDef2);
+		edgeShape.dispose();
 		// Shape is the only disposable of the lot, so get rid of it
 		shape1.dispose();
 		shape2.dispose();
+
+		collision();
+	}
+
+	private void collision() {
+		world.setContactListener(new ContactListener() {
+			@Override
+			public void beginContact(Contact contact) {
+				Body fA = contact.getFixtureA().getBody();
+				Body fB = contact.getFixtureB().getBody();
+				if((fA == body2 && fB == body1) ||
+						(fA == body1 && fB == body2)) {
+					body1.applyForceToCenter(0, 600,true);
+
+
+					System.out.println("CONTACT " + "bodyC: " + body1.getPosition().x + " " + body1.getPosition().y);
+					System.out.println(""+fA+" "+fB);
+				}
+			}
+
+			@Override
+			public void endContact(Contact contact) {
+			}
+
+			@Override
+			public void preSolve(Contact contact, Manifold oldManifold) {
+			}
+
+			@Override
+			public void postSolve(Contact contact, ContactImpulse impulse) {
+			}
+		});
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+		world.step(1f / 60f, 6, 2);
 		// Now update the spritee position accordingly to it's now updated Physics body
 		spriteBall.setPosition(body1.getPosition().x, body1.getPosition().y);
 
@@ -106,66 +159,27 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 		batch.draw(spriteBackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.enableBlending();
 		batch.draw(spriteBall, spriteBall.getX(), spriteBall.getY());
-		//
+
 		batch.draw(texturePlatform,
 				gameModel.getMap().getMainPlatform().getHeight()
 				, 0,
 				gameModel.getMap().getMainPlatform().getWidth(),
 				gameModel.getMap().getMainPlatform().getHeight());
-		roll();
-		//jump();
-		System.out.println("X: "+ spriteBall.getX()+" Y: "+ spriteBall.getY());
+		//roll();
+
+		//System.out.println("X: "+ spriteBall.getX()+" Y: "+ spriteBall.getY());
+		//System.out.println("X1: "+ body1.getPosition().x+" Y1: "+ body1.getPosition().y);
 		batch.end();
 
 	}
 
 	public void roll()
 	{
-
-		// y: -2 (left), 0 (still), 2 (right)
 		float adjustedY = Gdx.input.getAccelerometerY();
-		//if( adjustedY < - 2f ) adjustedY = - 2f; else if( adjustedY > 2f ) adjustedY = 2f;
-		// since 2 is 100% of movement speed, let's calculate the final speed percentage
-		//adjustedY /= 2;
-
-		// notice the inverted axis because the game is displayed in landscape mode()20 temporärer speed
-		//b.getSprite().setX(b.getSprite().getX() + adjustedY*20f );
-		body1.setLinearVelocity(adjustedY*40, 0);
-
+		body1.setLinearVelocity(adjustedY * 40, 0);
 
 	}
-	//prüft JumpStatus der Kugel fals er nicht auf standing ist wird ein Sprung oder ein Fall ausgeführt
-	/**public void jump()
-	{
-		JumpStates j = b.getJumpState();
-		float speed = b.getJumpSpeed();
-		if (j == JumpStates.ASCENDING)
-		{
-			//We're ascending. Decrease the Y coordinate of the sprite by the speed.
-			//DecreaseYCoordinateBy(Speed);
-			b.setyCoordinates(b.getyCoordinates()+b.getJumpSpeed());
-			b.setJumpSpeed(b.getJumpSpeed()-1);
-			// DecreaseByValue(Speed, Value); //The character needs to jump smoothly, so the speed should decrease as he ascends.
-			if (b.getJumpSpeed() <= 0)
-			{
-				b.setJumpState(JumpStates.DESCENDING);//If speed is <= 0, then the character should fall down.
-				b.setJumpSpeed(0);
-			}
-		}
-		else if (j == JumpStates.DESCENDING)
-		{
-			//We're descending. Increase the Y coordinate by the speed (at first, it's 0).
-			b.setyCoordinates(b.getyCoordinates()-b.getJumpSpeed());
-			b.setJumpSpeed(b.getJumpSpeed()+1); //Increase the speed, so the character falls gradually faster.
-			if (b.getyCoordinates() >= b.getyCoordinatesOriginal())
-			{
-				//If we reached the original Y coordinate, we hit the ground. Mark the character as standing.
-				b.setJumpState(JumpStates.STANDING);
-				b.setyCoordinates(b.getyCoordinatesOriginal());
-				b.setJumpSpeed(b.getOriginalJumpSpeed());
-			}
-		}
-	}**/
+
 
 
 	@Override
@@ -186,16 +200,9 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		Gdx.app.log("Controller", "pressed Jump");
-		/**Ball b = gameModel.getPlayer().getBall();
-		 Gdx.app.log("Controller", "getJumpSpeed : "+b.getJumpSpeed()+" getOriginalJumpSpeed : "+b.getOriginalJumpSpeed()
-		 +" getJumpState : "+b.getJumpState()+" getyCoordinates : "+b.getyCoordinates()
-		 +" getyCoordinatesOriginal : "+b.getyCoordinatesOriginal());
-		 if(b.getJumpState() != JumpStates.ASCENDING  || b.getJumpState() != JumpStates.DESCENDING   )
-		 {
-		 b.setJumpState(JumpStates.ASCENDING);
-		 b.setyCoordinatesOriginal(b.getyCoordinates());
-		 }**/
-		body1.setLinearVelocity(0, 50f);
+
+		body1.setLinearVelocity(0, 300f);
+
 
 		return false;
 	}
